46c64b04a1f8625584f927fee528ba2f74043660
eugenia0804 <67468941+eugenia0804@users.noreply.github.com>
2023-05-22 15:48:42 -0500

    change the prompt into a json format

--- codebookinfo.py
+++ codebookinfo.py
@@ -7,19 +7,6 @@
     returns a prompt section that explain the practice details as a string.
 
 """
-def get_codebookinfo1(dic,index):
-        # Extract the name of the practice at the given index
-        practice_name = list(dic.keys())[index-1]
-        # Extract the list of subpractices for the given practice name and join them in a single line
-        subpractices_list = dic[practice_name]['Subpractices']
-        subpractices = "\n'"+"',\n'".join(subpractices_list)
-        # Extract the list of examples for the given practice name and join them in a single line
-        examples_list = dic[practice_name]['Examples']
-        examples = "\n'"+"',\n'".join(examples_list)
-        
-        # Form up the composite prompt used to feed in GPT.
-        prompt = f"The name of the practice is '{practice_name}', which is consisted of subpractices includes {subpractices}.\n \nExample question which reflect this practice includes: {examples}.\n \n"
-        return prompt
 
 def get_codebookinfo(dic,index):
         # Extract the name of the practice at the given index
