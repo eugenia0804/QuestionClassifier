4fd4993c89f184ff11692d596e3b1d720fa3fb7e
eugenia0804 <67468941+eugenia0804@users.noreply.github.com>
2023-06-06 13:28:12 -0500

    add utils funcs

--- questionanswer.py
+++ questionanswer.py
@@ -1,4 +1,7 @@
 from code.utils.question import get_questions
 import pandas as pd
-import json
+
+
+
+
 
--- questioninfo.py
+++ questioninfo.py
@@ -7,7 +7,7 @@
 """
 from code.utils.question import get_questions
 import pandas as pd
-import json
+
 
 template = {
     "Question_number": {
